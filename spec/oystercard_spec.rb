c8afc40e31537d764b5f7d185b3f4f2057c2e9f8
Chris Wynne <chris.wynndow@gmail.com>
2016-01-11 16:55:12 +0000

    Adds a starting balance for the card

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,13 @@
+require 'oystercard.rb'
+
 describe Oystercard do
+
+  subject(:oyster) {described_class.new}
+
+  describe 'By default' do
+    it 'has a starting balance of 0' do
+      expect(oyster.balance).to eq(0)
+    end
+  end
 
 end
